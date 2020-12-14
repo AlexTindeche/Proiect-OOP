c61f855707fab1de7a781729e61b062ab5f4837e
Tudor Butufei <tudor.butufei@gmail.com>
2020-12-14 14:07:01 +0200

    inceput idei

--- ProiectOOP-AC Phase1/main.cpp
+++ ProiectOOP-AC Phase1/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
 
+/**
+* Component
+* - de baza: Rezistenta, Bobina, Condensator
+* - compus
+* 
+* - operatii
+*   -> adunare (care poate fi in serie sau in paralel)
+* 
+* Circuit
+* - contine mai multe componente legate intr-un anumit mod
+* - 
+* 
+* 
+* Un model de functionare la care ma gandeam:
+* definim componente
+* R1 = Rezistenta(10);
+* R2 = Rezistenta(20);
+* C1 = Condensator(342);
+* 
+* ((R2 + R1 + C2) * (R3 + ...) + r3) * (R4 + L3)
+*/
+
 int main()
 {
-	char c[20000];
-	cin >> c;
-	cout << c;
+	
 	return 0;
 
 }
